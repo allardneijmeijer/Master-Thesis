7833824b985f90a3c711c6159af3348d9e01c0d3
Allard Neijmeijer <allardneijmeijer@gmail.com>
2015-10-19 12:07:28 +0200

    Simulator built from the ground up. Half functioning

--- Simulator.py
+++ Simulator.py
@@ -1,5 +1,3 @@
-__author__ = 'allardneijmeijer'
-
 from scipy.stats import expon
 #from collections import defaultdict
 from sortedcontainers import SortedSet
@@ -9,6 +7,11 @@ np.random.seed(1)
 
 
 class Event(object):
+    """
+    The Event class is used to represent all events happening in the simulator. The events are stored in the event stack
+    and are sorted on time of execution. All events have a Job associated with it. Events can be of several different kinds:
+    Arrivals, Start of processing, stop of processing, failures, etc....
+    """
     __slots__ = ['time', 'event', 'job', 'ql', 'qt', 'server']
 
     def __init__(self, time, event, job):
@@ -18,26 +21,33 @@ class Event(object):
         self.job = job          # Use instance of class Job for this event
 
     def __str__(self):
-        return "The event starts at t={}".format(self.time)
+        return "The event is a '{}', concerned with job {} ".format(self.event, self.job.time)
 
 
-class Job(object):              # Jobs
+class Job(object):
+    """
+    The Job class describes every job that traverses the simulated system. The jobs have several parameters (to be described later)
+    """
     __slots__ = ['nr', 'arrival', 'service', 'a', 'qt', 's', 'jobtype', 'server']
 
     def __init__(self, nr, a, s, jobtype, server):
 
-        self.nr = nr
-        self.arrival = a
-        self.service = s
-        self.jobtype = jobtype
-        self.server = server
-        self.qt = 0
+        self.nr = nr                # job nr
+        self.arrival = a            # arrival time of job
+        self.service = s            # service time of job
+        self.jobtype = jobtype      # typ of job (processing, maintenance, repair)
+        self.server = server        # server where the job needs processing ( may change to become a list/dictionary later for more servers)
+        self.qt = 0                 # total queueing time of job
 
     def __str__(self):
-        return "Job is being served"
+        return "Job nr. {} is being served".format(self.nr)
 
 
-class Queue(SortedSet):            # Queues are a storage of jobs before a server
+class Queue(SortedSet):
+    """
+    The Queue class is used to represent the queues for all servers. Queues contain Jobs that have arrived at a certain server, but cannot be processed immediately.
+    The jobs are stored in the order of their arrival (later, different queueing algortithms will be implemented)
+    """
     def __init__(self):
         super().__init__()
         self.sup = super(Queue, self)
@@ -46,11 +56,16 @@ class Queue(SortedSet):            # Queues are a storage of jobs before a serve
         self.pop = self.sup.pop
 
 
-class Server(Machine):           # Server processes the jobs.
+class Server(Machine):
+    """
+    Class server is describing the servers of the system. The servers are modelled using finite state machines. Each server
+    has associated a current state, and a fixed set of possible state transitions. The state transitions trigger functions
+    that keep the simulator running.
+    """
     def __init__(self):
         state = ['Processing', 'Idle', 'Maintenance', 'Failure']
         Machine.__init__(self, states=state, initial='Idle')
-        self.add_transition('repair', 'Failure', ['Idle', 'Processing'])  # , after='generateFailure',before = '')
+        self.add_transition('repair', 'Failure', ['Idle', 'Processing'])
         self.add_transition('fail', ['Processing', 'Idle'], 'Failure')
         self.add_transition('start', 'Idle', 'Processing', before='startprocessing')
         self.add_transition('stop', 'Processing', 'Idle', after='stopprocessing')
@@ -59,31 +74,44 @@ class Server(Machine):           # Server processes the jobs.
         self.activeJob = None
         self.queue = Queue()        # will be implemented differently in G\G\n
 
-    def startprocessing(self,job):
+    def startprocessing(self, job):
+        """
+        This function starts processing of the specified job. It sets the current activejob of the server to this job.
+        :param job:
+        :return:
+        """
         self.activeJob = job
 
     def stopprocessing(self):
-        exit = self.activeJob
-        self.activejob = None
-        return exit
+        """
+        This function sets the current activejob to None. Next, it checks if the queue is non-empty, and if this is true
+        it triggers a state-transition of the machine to process the first job in the queue.
+        :return:
+        """
+        self.activeJob = None
+        if self.queue:
+            self.start(self.queue.pop(0))
 
     def generateFailure(self):
+        """
+        To be implemented
+        :return:
+        """
         pass
 
     def generateMaintenance(self):
+        """
+        To be implemented
+        :return:
+        """
         pass
 
 
-# class ProcessingStep(object):
-# to be implemented later, when G/G/n
-#     #contains the servers and their queue(s)
-#     def __init__(self):
-#         self.queue = Queue()
-#         # For now: one queue, one server:
-
-
-
-class Stack(SortedSet):            # Stack stores all events in a sortedset
+class Stack(SortedSet):
+    """
+    The Stack class is the core of the simulator. The event stack is implemented by using a sortedset, sorting all events
+    on their execution time. The event stack also contains the simulation time.
+    """
     def __init__(self):
         super().__init__()
         self.sup = super(Stack, self)
@@ -94,26 +122,58 @@ class Stack(SortedSet):            # Stack stores all events in a sortedset
         self.now = 0
 
     def addJobs(self, jobs):
+        """
+        This function adds the specified list of jobs to the event stack. This function is used to initialize the simulation.
+        :param jobs:
+        :return:
+        """
         for nr,job in enumerate(jobs):
             self.stackadd( Event(job.arrival, 'a', job) )
 
-    def addEvent(self,time,event,job):
-        self.stackadd( Event(time = time,event = event, job = job) )
+    def addEvent(self, time, event, job):
+        """
+        Create a new event to be executed at the specified time, concerning the specified job.
+        :param time:
+        :param event:
+        :param job:
+        :return:
+        """
+        self.stackadd( Event(time = time, event = event, job = job) )
 
     def run(self):
+        """
+        Performs the actual simulation. While the eventstack is nonempty, this function takes the first event on the stack
+        and executes this.
+        :return:
+        """
         while self:
-            #print(len(self))
             a = self.stackpop(0)
-            #print (a.time)
+
+            #debugging
+            #print(a.job.server.state)
+            #print(a.event)
+            #print(self.now)
+
+            self.now = a.time
             if a.event == 'a' and a.job.server.is_Idle():
                 a.job.server.start(a.job)
+                tstop = a.job.service + self.now
+                self.addEvent(tstop, 'f', a.job)
+                self.printCont()
             elif a.event == 'a' and not(a.job.server.is_Idle()):
                 a.job.server.queue.add(a.job)
+                #print(len(a.job.server.queue))
+            elif a.event == 'f':
+                a.job.server.stop()
             elif a.event == 'm':
                 pass
             elif a.event == '':
                 pass
 
+    def printCont(self):
+        b = list(iter(self))
+        for i,item in enumerate(b):
+            print(b[i].time, b[i].job.service, b[i].event)
 
 def makeJobs(num, F, G,serv):
         jobs = list()
@@ -136,10 +196,3 @@ s.addJobs(jobs)
 #s.stackadd(Event(-1, 1, 1))
 
 s.run()
-
-#To be used to inspect list!
-
-# b = list(iter(s))
-#
-# for i,item in enumerate(b):
-#     print(b[i].event)
